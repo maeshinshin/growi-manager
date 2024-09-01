e79c60e989fe8e75783d0f419bc5b35a1fb5fbca
maekawa_shintaro <maeshinshin@outlook.jp>
2024-09-01 22:26:16 +0900

    rename api name

--- internal/controller/growi_controller.go
+++ internal/controller/growi_controller.go
@@ -33,9 +33,9 @@ type GrowiReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=maeshinshin.github.io.maeshinshin.github.io,resources=growis,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=maeshinshin.github.io.maeshinshin.github.io,resources=growis/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=maeshinshin.github.io.maeshinshin.github.io,resources=growis/finalizers,verbs=update
+// +kubebuilder:rbac:groups=maeshinshin.github.io,resources=growis,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=maeshinshin.github.io,resources=growis/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=maeshinshin.github.io,resources=growis/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
