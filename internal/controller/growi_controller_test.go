6ee64fdc37adafdf698fa5240ff68d7f98d8a578
maesh <maesh.esh.sh@gmail.com>
2025-05-11 04:14:18 +0900

    add test

--- internal/controller/growi_controller_test.go
+++ internal/controller/growi_controller_test.go
@@ -53,16 +53,6 @@ var _ = Describe("Growi Controller", func() {
 			),
 			Namespace: testNamespaceName,
 		}
-		mongoDBSecretTypeNamespcedName := types.NamespacedName{
-			Name: getMongoDBSecretName(
-				appv1.Growi{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: resourceName,
-					},
-				},
-			),
-			Namespace: namespaceName,
-		}
 		growi := &appv1.Growi{}
 		mongoDBSecret := &corev1.Secret{}
 
