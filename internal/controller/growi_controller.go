41daa24a53d556146e154f4dcb68fcfa221632d4
maesh <maesh.esh.sh@gmail.com>
2025-05-21 16:06:44 +0900

    add elasticsearch

--- internal/controller/growi_controller.go
+++ internal/controller/growi_controller.go
@@ -46,6 +46,7 @@ type GrowiReconciler struct {
 
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
